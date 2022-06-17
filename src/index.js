587dbc84c7528ad98e80f7af8dd53a057e596af5
Ruby <52339819+betweenbrackets@users.noreply.github.com>
2022-06-17 12:54:47 -0700

    Render the NameTag Functional Component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+import NameTag from "./NameTag.js";
+
 const App = () => (
   <div className="App">
-    <h1>Hello Name Tag Generator</h1>
+    <h1>Name Tag Generator</h1>
+    <NameTag />
   </div>
 );
 
