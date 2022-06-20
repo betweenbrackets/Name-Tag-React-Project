5385bf5e32334114ef937dd9a60396fc47048b12
Ruby <52339819+betweenbrackets@users.noreply.github.com>
2022-06-20 16:19:37 -0700

    Build the Names Data Model

---
+++ src/data.js
@@ -0,0 +1,9 @@
+// named export
+
+export const names = [
+  { person: "Boethius", id: "001" },
+  { person: "Anselm", id: "002" },
+  { person: "Aristotle", id: "003" },
+  { person: "Plato", id: "004" },
+  { person: "Aquinas", id: "005" }
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+// import of a named export
+import { names } from "./data.js";
 
 import "./styles.css";
 
-import NameTag from "./NameTag.js";
+// write the callback function that will accept a name and returns a NameTag component
+// this is tricky: the callback function handles a single array object. The array is names. The single array object is name. Name is the prop that passes the names array attributes.
+const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Boethius" />
-    <NameTag name="Aristotle" />
-  </div>
-);
+const App = () => {
+  // explicit return (more than one)
+  // map over the array
+  const NameTagElements = names.map(renderNameTag);
+
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
